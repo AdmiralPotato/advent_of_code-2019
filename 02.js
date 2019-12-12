ba8f988c7194e5e794fcc140d94e303e14b5a58f
Admiral Potato <admiral@nuclearpixel.com>
2019-12-11 19:36:45 -0800

    Solve Puzzle 02 part 2 in JS

--- 02.js
+++ 02.js
@@ -1,32 +1,70 @@
 const fs = require('fs')
 const dataPath = './02-input.txt'
 let input = fs.readFileSync(dataPath, 'utf8')
-let instructions = input.split(',').map((string)=> parseInt(string, 10))
-console.log(instructions)
 
-// extra garbage instructions at bottom relating to "1202" error
-instructions[1] = 12
-instructions[2] = 2
+const runIntcode = (instructions, noun, verb) => {
+    instructions[1] = noun
+    instructions[2] = verb
+    let currentInstructionIndex = 0
+    let currentInstruction = instructions[currentInstructionIndex]
+    while(currentInstruction !== 99) {
+        operandA = instructions[instructions[currentInstructionIndex + 1]]
+        operandB = instructions[instructions[currentInstructionIndex + 2]]
+        let result
+        if (currentInstruction === 1) {
+            result = operandA + operandB
+        } else if(currentInstruction === 2) {
+            result = operandA * operandB
+        } else {
+            console.warn('BAD TIMES, BAD OPCODE: ' + currentInstruction)
+            console.log({
+                instructions: instructions.join(','),
+                currentInstructionIndex,
+                currentInstruction,
+                operandA,
+                operandB,
+                result
+            })
+            break
+        }
+        instructions[instructions[currentInstructionIndex + 3]] = result
 
-let currentInstructionIndex = 0;
-let currentInstruction = instructions[currentInstructionIndex]
-
-while(currentInstruction !== 99) {
-    operandA = instructions[instructions[currentInstructionIndex + 1]]
-    operandB = instructions[instructions[currentInstructionIndex + 2]]
-    let result
-    if (currentInstruction === 1) {
-        result = operandA + operandB
-    } else if(currentInstruction === 2) {
-        result = operandA * operandB
-    } else {
-        throw 'BAD TIMES, BAD OPCODE: ' + currentInstruction
+        currentInstructionIndex += 4
+        currentInstruction = instructions[currentInstructionIndex]
     }
-    instructions[instructions[currentInstructionIndex + 3]] = result
-
-    currentInstructionIndex += 4
-    currentInstruction = instructions[currentInstructionIndex]
-}
-console.log('Program end on instruction index:', currentInstructionIndex)
-console.log('Program end on opcode:', (currentInstructionIndex / 4) + 1)
+    // console.log(instructions.join(','))
+    return instructions
+} 
+const pureInstructions = input.split(',').map((string)=>parseInt(string, 10)) 
+let instructions = pureInstructions.slice()
 console.log(instructions.join(','))
+
+console.log('Initial run with noun, verb as 12, 02')
+runIntcode(instructions, 12, 2)
+
+const intendedResult = 19690720
+let lastResult = null
+let i = 1
+let j = 1
+while (i < 100 && lastResult !== intendedResult) {
+    while (j < 100 && lastResult !== intendedResult) {
+        instructions = pureInstructions.slice()
+        runIntcode(instructions, i, j)
+        lastResult = instructions[0]
+        console.log(`noun: ${i}, verb: ${j}, result: ${lastResult}`)
+        if(lastResult === intendedResult) {
+            break
+        }
+        j++
+    }
+    if(lastResult === intendedResult) {
+        break
+    }
+    i++
+    j = 1
+}
+if(lastResult !== intendedResult) {
+    console.error('Combination not found. Something is wrong.')
+} else {
+    console.log(`Correct result found! noun: ${i}, verb: ${j}, answer: ${(i * 100) + j}`)
+}
